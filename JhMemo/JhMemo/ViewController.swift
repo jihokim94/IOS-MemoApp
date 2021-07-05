991966c8be6f94cd53ccb20504aaac7ae96eb44a
jihoKim94 <kj941225@gmail.com>
2021-07-06 00:07:22 +0900

    메모앱 아이콘,런치 스크린 생성

---
+++ JhMemo/JhMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JhMemo
+//
+//  Created by 김지호 on 2021/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
