6dbb8a1e5b8e8f0159c720de3df1dbe45db07779
jihoKim94 <kj941225@gmail.com>
2021-07-06 16:28:59 +0900

    더미리스트 출력 및 데이트 포맷

---
+++ JhMemo/JhMemo/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  JhMemo
+//
+//  Created by 김지호 on 2021/07/06.
+//
+
+import Foundation
+
+class Memo {
+    var content : String //메모내용
+    var insertDate : Date // 작성날짜
+    
+    init(content : String) {
+        self.content = content
+        self.insertDate = Date()
+    }
+   
+    
+    static var dummyMemoList = [
+        Memo(content: "더미입니다"),
+        Memo(content: "두번째 더미입니다 🍉")
+    ]
+}
