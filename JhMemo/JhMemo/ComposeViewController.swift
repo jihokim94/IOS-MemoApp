a29a90f2f0e95482372b9023cad89d9d8fc4021b
jihoKim94 <kj941225@gmail.com>
2021-07-08 18:54:02 +0900

    메모 수정 구현 및 노티피케이션 추가

--- JhMemo/JhMemo/ComposeViewController.swift
+++ JhMemo/JhMemo/ComposeViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ComposeViewController: UIViewController {
     
+    var editTarget : Memo?
+    
     @IBAction func close(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil) // 프레젠트 모달창을 끌때 쓰는 메소드
         // 꺼지면서 뭐가 있어할 건아니니깐 nil
@@ -24,12 +26,23 @@ class ComposeViewController: UIViewController {
             return
         }
         
+        
 //        let newMemo = Memo(content: memo)
 //        Memo.dummyMemoList.append(newMemo)
-        DataManager.shared.addNewMemo(memo)
+        if let target = editTarget { // 편집일 경우
+            target.content = memo
+            DataManager.shared.saveContext()
+            NotificationCenter.default.post(name: ComposeViewController.memoDidUpdate, object: nil)
+        } else { // 일반적인 메모 추가 경우
+            DataManager.shared.addNewMemo(memo)
+            NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+        }
         
         
-        NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+       
+        
+        
+        
         
         dismiss(animated: true, completion: nil)
         
@@ -40,6 +53,13 @@ class ComposeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let memo = editTarget {
+            navigationItem.title = "메모 편집"
+            memoTextView.text = memo.content
+        } else {
+            navigationItem.title = "새 메모"
+            memoTextView.text = ""
+        }
         // Do any additional setup after loading the view.
     }
     
@@ -59,4 +79,5 @@ class ComposeViewController: UIViewController {
 }
 extension ComposeViewController {
     static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
+    static let memoDidUpdate = Notification.Name(rawValue: "memoDidUpdate")
 }
